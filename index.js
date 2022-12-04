e52dad136336cebc99cba3150a2fa70ae2ef197b
hiten <hitenkakwani400@gmail.com>
2022-12-04 11:54:46 +0530

    my first production release

---
+++ index.js
@@ -0,0 +1,42 @@
+// console.log("hello");
+const express = require('express');
+const app = express();
+const PORT = 4000 || process.env.PORT;
+const format = require('date-format')
+app.get("/",(req,res)=>{
+    res.send("Hello")
+});
+app.get("/api/v1/instagram",(req,res)=>{
+    const instaSocial={
+        username:"hiten__",
+        followers:557,
+        follows:604,
+        date:format.asString("dd[MM] - hh:mm:ss",new Date()),
+    };
+    res.status(200).json(instaSocial);
+});
+app.get("/api/v1/facebook",(req,res)=>{
+    const instaSocial={
+        username:"hiten__",
+        followers:557,
+        follows:604,
+        date:format.asString("dd[MM] - hh:mm:ss",new Date()),
+    };
+    res.status(200).json(instaSocial);
+});
+app.get("/api/v1/linkedIn",(req,res)=>{
+    const instaSocial={
+        username:"hiten__",
+        followers:557,
+        follows:604,
+        date:format.asString("dd[MM] - hh:mm:ss",new Date()),
+    };
+    res.status(200).json(instaSocial);
+});
+app.get("/api/v1/:token",(req,res)=>{
+    console.log(req.params.token);
+    res.status(200).json({param:req.params.token});
+})
+app.listen(PORT,()=>{
+    console.log(`Server is running at ${PORT}`);
+});
